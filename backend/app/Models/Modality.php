d97c77415e775e8195257076efb5ca6a211b8bf7
Jihad <Jihad.jihad@ski-planet.com>
2026-07-07 14:38:38 +0200

    refactor: retirer les relations inverses AESH non utilisées (code mort)

--- backend/app/Models/Modality.php
+++ backend/app/Models/Modality.php
@@ -3,14 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Modality extends Model
 {
     protected $fillable = ['slug', 'name'];
-
-    public function aeshProfiles(): BelongsToMany
-    {
-        return $this->belongsToMany(AeshProfile::class);
-    }
 }
